640aaade529114da435ddcc0882317e4d20c5b46
grechanyuk <xorolllo@hotmail.com>
2019-05-23 10:41:14 +0500

    Added currency icon column

--- src/Models/CentralBankCurrency.php
+++ src/Models/CentralBankCurrency.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class CentralBankCurrency extends Model
 {
-    protected $fillable = ['name', 'par', 'cb_code', 'iso_code', 'value'];
+    protected $fillable = ['name', 'par', 'cb_code', 'iso_code', 'value', 'icon'];
 }
